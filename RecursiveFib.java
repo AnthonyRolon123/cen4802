1b5966958ec2d58cb060072243b0da0b66a9872f
AnthonyRolon123 <anthonyrolon1234@gmail.com>
2023-09-05 19:38:37 -0400

    Update RecursiveFib.java

--- RecursiveFib.java
+++ RecursiveFib.java
@@ -1,10 +1,19 @@
 public class RecursiveFib
 {
+    /**
+     * Main Method
+     * @param args
+     */
     public static void main(String []args)
     {
         System.out.println("The 10th element is " + fibonacci(10));
     }
 
+    /**
+     * Method used to recursively call itself and add onto itself until n is 0
+     * @param n
+     * @return fibonacci method
+     */
     public static int fibonacci(int n)
     {
         if(n == 0)
